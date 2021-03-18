aa1681b58fc8574958d83ff76785095848711513
xiadd <xiadd0102@gmail.com>
2021-03-18 11:11:03 +0800

    fix

--- src/pages/assets/index.jsx
+++ src/pages/assets/index.jsx
@@ -2,7 +2,7 @@ import AssetCard from '@/components/asset-card'
 import useGetAssets from '@/data/use-get-assets'
 
 export default function Index () {
-  const { data, loading, error } = useGetAssets({ refreshInterval: 10000 })
+  const { data } = useGetAssets({ refreshInterval: 10000 })
   return (
     <div className="container h-full mx-auto p-5 pt-10 grid grid-flow-row grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-4">
       {data?.data?.map(item => <AssetCard key={item.id} item={item} />)}
